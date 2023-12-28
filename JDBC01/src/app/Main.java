50f8d5fc1d87ac6c0975362d3e5fcbf0d55002ff
Mateus Flores <mateusgf.42@gmail.com>
2023-12-28 12:11:16 -0300

    consultas JDBC

--- JDBC01/src/app/Main.java
+++ JDBC01/src/app/Main.java
@@ -1,12 +1,41 @@
 package src.app;
 
 import src.db.DB;
+import src.db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        selectDepartments();
     }
+
+    private static void selectDepartments() {
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+            st.executeQuery("use coursejdbc");
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException sqle) {
+            throw new DbException(sqle.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+
 }
