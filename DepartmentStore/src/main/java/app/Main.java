2996dbc06a5e1a0d5615471bff1a758ca98c529f
Mateus Flores <mateusgf.42@gmail.com>
2024-01-05 00:11:11 -0300

    SellerDaoJDBC e DaoFactory

--- DepartmentStore/src/main/java/app/Main.java
+++ DepartmentStore/src/main/java/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Tech");
         System.out.println(obj.toString());
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.00, obj);
         System.out.println(seller.toString());
