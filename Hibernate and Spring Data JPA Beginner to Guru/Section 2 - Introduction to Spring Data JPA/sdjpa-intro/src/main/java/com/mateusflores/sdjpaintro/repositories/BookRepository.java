1f1830f01c49bc041f76c83b6600f2e2e85c0a8d
Mateus Flores <mateusgf.42@gmail.com>
2024-09-02 14:22:32 -0300

    remocao de curso descontinuado

--- Hibernate and Spring Data JPA Beginner to Guru/Section 2 - Introduction to Spring Data JPA/sdjpa-intro/src/main/java/com/mateusflores/sdjpaintro/repositories/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mateusflores.sdjpaintro.repositories;
-
-import com.mateusflores.sdjpaintro.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
